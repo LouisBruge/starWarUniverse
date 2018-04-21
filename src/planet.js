76af035336e15d0bfb5efcfb375247b384aaf8e9
griselangue <louis.bruge@gmail.com>
2018-04-22 00:09:23 +0200

    Create a fetch method to load data on the planet component

--- src/planet.js
+++ src/planet.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
 
-export default class planet extends {
+export default class planet extends Component {
     constructor () {
         super()
 
-        state = {
+        this.state = {
             isLoading : true,
             planet: {}
         }
     }
+
+    _fetchPlanet () {
+        fetch('https://swapi.co/api/planets/2/')
+            .then( function (response) {
+                return response.json()
+            })
+            .then ( (myJSON) =>  {
+                this.setState({
+                    isLoading: false
+                })
+                console.log(myJSON)
+                console.log(this.state.isLoading)
+            })
+    }
+
+    componentWillMount() {
+        this._fetchPlanet()
+    }
+
     render () {
+        if ( this.state.isLoading) {
+            return (
+                <div>
+                    <p> Wait, Please. It's loading...</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <p> OK </p>
+                </div>
+            )
+        }
 
     }
 }

e203e96de8b34218da16cd8b2b235892c42cec02
griselangue <louis.bruge@gmail.com>
2018-04-21 21:59:22 +0200

    Create the first component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <div>
+            <h1> Star War Univers </h1>
+            </div>
+        ) 
+    }
+}

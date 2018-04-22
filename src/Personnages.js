9d6db7a473fd7bc45bf7134fc92fccc48d66b242
Louis Bruge <louis.bruge@gmail.com>
2018-04-22 20:23:57 +0200

    Create a fetch mehtod for the clas Personnage

--- src/Personnages.js
+++ src/Personnages.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+const URL_PEOPLE = 'https://swapi.co/api/people/'
 
 export default class Personnage extends Component {
   constructor () {
@@ -8,5 +9,15 @@ export default class Personnage extends Component {
       isLoading: true,
       personnage: {}
     }
+  }
+
+  _fetchPersonnage () {
+    fetch(URL_PEOPLE + '1')
+      .then((response) => {
+        return response.json()
+      })
+      .then((myJson) => {
+        console.log(myJson)
+      })
   }
 }

5477a35a6294c9134212825decf5dcd19ba9c584
Louis Bruge <louis.bruge@gmail.com>
2018-04-22 20:45:05 +0200

    Insert the Personnage component to the App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Planet from './planet.js'
+import Personnage from './Personnages.js'
 
 export default class App extends React.Component {
-    render () {
-        return (
-            <div>
-            <h1> Star War Univers </h1>
-            <Planet />
-            </div>
-        ) 
-    }
+  render () {
+    return (
+      <div>
+        <h1> Star War Univers </h1>
+        <Planet />
+        <Personnage />
+      </div>
+    )
+  }
 }

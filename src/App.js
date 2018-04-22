4b733ecc74c34919418d2691f9da9e8eaa17b33f
griselangue <louis.bruge@gmail.com>
2018-04-22 16:53:49 +0200

    Add planet component to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Planet from './planet.js'
 
 export default class App extends React.Component {
     render () {
         return (
             <div>
             <h1> Star War Univers </h1>
+            <Planet />
             </div>
         ) 
     }

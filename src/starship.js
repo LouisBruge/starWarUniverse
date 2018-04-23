9c8284a5b34ef05144bae8b870091a5e201590fa
Louis Bruge <louis.bruge@gmail.com>
2018-04-23 11:07:54 +0200

    Create a fetch method for the class Starship

--- src/starship.js
+++ src/starship.js
@@ -1,4 +1,5 @@
 import React from 'react'
+const URL_STARSHIP = 'https://swapi.co/api/starships/'
 
 export default class Starship extends React.Component {
   constructor () {
@@ -8,6 +9,19 @@ export default class Starship extends React.Component {
       isLoading: true,
       starship: {}
     }
+  }
+
+  _fetchStarship () {
+    fetch(URL_STARSHIP + '4')
+      .then(function (response) {
+        return response.json()
+      })
+      .then((myJSON) => {
+        this.setState({
+          startship: myJSON
+        })
+        console.log(this.state.startship)
+      })
   }
 
   render () {

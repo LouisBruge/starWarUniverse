68d5fa434cacd658d1b50ece1656913e83515cb7
Louis Bruge <louis.bruge@gmail.com>
2018-04-28 13:45:37 +0200

    Add the react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import Planet from './planet.js'
+import Starship from './starship'
 
 export default class App extends React.Component {
-    render () {
-        return (
-            <div>
-            <h1> Star War Univers </h1>
-            <Planet />
-            </div>
-        ) 
-    }
+  render () {
+    return (
+      <div>
+        <h1> Star War Univers </h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/planet' component={Planet} />
+            <Route exact path='/starship' component={Starship} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }

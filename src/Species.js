5b4ac0dc8475d865487ab92f80ee1bc794cbd9dc
Louis Bruge <louis.bruge@gmail.com>
2018-04-30 13:58:14 +0200

    edit the constructor method of the Species class

--- src/Species.js
+++ src/Species.js
@@ -3,5 +3,9 @@ import React from 'react'
 export default class Species extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      isLoading: true,
+      species: {}
+    }
   }
 }

5957644771622105a11d482c2468617de778a890
Louis Bruge <louis.bruge@gmail.com>
2018-04-30 14:02:27 +0200

    Add a fetchSpecies method to the Species class

--- src/Species.js
+++ src/Species.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+const URL_SPECIES = 'https://swapi.co/api/species'
+
 export default class Species extends React.Component {
   constructor (props) {
     super(props)
@@ -7,5 +9,18 @@ export default class Species extends React.Component {
       isLoading: true,
       species: {}
     }
+  }
+
+  _fetchSpecies () {
+    fetch(URL_SPECIES + '3')
+      .then(function (response) {
+        return response.json()
+      })
+      .then(myJSON => {
+        this.setState({
+          isLoading: false,
+          species: myJSON
+        })
+      })
   }
 }

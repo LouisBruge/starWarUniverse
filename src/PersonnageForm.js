98e3c92efa140d5bec1149de4e60299b05086228
Louis Bruge <louis.bruge@gmail.com>
2018-04-30 20:34:32 +0200

    Create a method _urlToId inside the PersonnageForm class

--- src/PersonnageForm.js
+++ src/PersonnageForm.js
@@ -23,18 +23,19 @@ export default class PersonnageForm extends React.Component {
       })
   }
 
+  _urlToId (url) {
+    let reg = new RegExp(/(\d+)/)
+    let id = reg.exec(url)
+    return id[0]
+  }
+
   render () {
     if (this.state.results.length > 0) {
       for (let i = 0; i < this.state.results.length; i++) {
-        console.log(this.state.results)
-        let reg = new RegExp(/(\d+)/)
-
-        let id = reg.exec(this.state.results[i].url)
-        console.log(id)
-
+        let id = this._urlToId(this.state.results[i].url)
         return (
           <div>
-            <Personnage name={id[0]} />
+            <Personnage name={id} />
           </div>
         )
       }

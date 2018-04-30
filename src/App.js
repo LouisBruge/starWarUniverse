8024dd3a6e64f5379641e447a30ff8aac4c0da3f
Louis Bruge <louis.bruge@gmail.com>
2018-04-30 11:22:27 +0200

    Remove planet component from the index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Planet from './planet.js'
 import Navbar from './Navbar.js'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1> Star War Univers </h1>
         <Navbar />
-        <Planet />
       </div>
     )
   }

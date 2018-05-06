4749c179af0bbc4cd22fda9197598c668768302e
Louis Bruge <louis.bruge@gmail.com>
2018-05-06 18:23:19 +0200

    Add a spinner logo to the Loading component

--- src/Loading.js
+++ src/Loading.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
 
 export default class Loading extends React.Component {
   render () {
     return (
       <Grid>
         <Row>
-          <Col xs={12}>
-            <p ClassName='text-center'> Wait... </p>
+          <Col xs={12} className='block-center'>
+            <p className='text-center'>
+              <FontAwesome name='spinner' /> Wait...
+            </p>
           </Col>
         </Row>
       </Grid>

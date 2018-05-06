34ae3d7580a046967bd658a8777884b6fe984318
Louis Bruge <louis.bruge@gmail.com>
2018-05-06 14:05:08 +0200

    Fix the heigh of imgs on carousel

--- src/CarouselIndex.js
+++ src/CarouselIndex.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import { Carousel, Image } from 'react-bootstrap'
 
 export default class CarouselIndex extends React.Component {
   render () {
     return (
       <Carousel indicators={false}>
         <Carousel.Item>
-          <img responsive alt='Carousel starWar' src='/Images/1.jpg' />
+          <Image alt='Carousel starWar' src='/Images/1.jpg' responsive />
         </Carousel.Item>
         <Carousel.Item>
-          <img responsive alt='Carousel starWar' src='/Images/2.jpg' />
+          <Image responsive alt='Carousel starWar' src='/Images/2.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <img responsive alt='Carousel starWar' src='/Images/3.jpg' />
+          <Image responsive alt='Carousel starWar' src='/Images/3.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <img responsive alt='Carousel starWar' src='/Images/4.jpg' />
+          <Image responsive alt='Carousel starWar' src='/Images/4.jpg' />
         </Carousel.Item>
         <Carousel.Item>
-          <img responsive alt='Carousel starWar' src='/Images/5.jpg' />
+          <Image alt='Carousel starWar' src='/Images/5.jpg' responsive />
         </Carousel.Item>
       </Carousel>
     )

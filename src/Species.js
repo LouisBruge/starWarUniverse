778723f9fa9fb252c3f2822977e9d31c95b88a4b
Louis Bruge <louis.bruge@gmail.com>
2018-05-09 13:02:41 +0200

    Use bootstrap component in the Species render and update it

--- src/Species.js
+++ src/Species.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Loading from './Loading'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 const URL_SPECIES = 'https://swapi.co/api/species/'
 
@@ -36,13 +37,29 @@ export default class Species extends React.Component {
       )
     } else {
       return (
-        <div>
-          <h2> {this.state.species.name} </h2>
-          <ul>
-            <li> Language : {this.state.species.language} </li>
-            <li> Classification : {this.state.species.classification} </li>
-          </ul>
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12} className='text-center'>
+              <h2> {this.state.species.name} </h2>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Classification : {this.state.species.classification} </p>
+              <p> Designation : {this.state.species.designation} </p>
+              <p> Lifespan : { this.state.species.average_lifespan } years</p>
+              <p> Language : {this.state.species.language } </p>
+              <p> Homeland : {this.state.species.homeworld} </p>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <h3> Morphology </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Average height : {this.state.species.average_height} cm</p>
+              <p> Eyes Colors : {this.state.species.eye_colors} </p>
+              <p> Hairs Colors : {this.state.species.hair_colors} </p>
+              <p> Skin Colors : {this.state.species.skin_colors} </p>
+            </Col>
+          </Row>
+        </Grid>
       )
     }
   }

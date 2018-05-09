d4f11da657b34a01dfee90d99ca046e67b3e770b
Louis Bruge <louis.bruge@gmail.com>
2018-05-09 12:28:18 +0200

    Use bootstrap component in the Vehicle render and update it

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Loading from './Loading'
+import { Grid, Col, Row } from 'react-bootstrap'
 
 const URL_VEHICLE = 'https://swapi.co/api/vehicles/'
 
@@ -36,13 +37,39 @@ export default class Vehicle extends React.Component {
       )
     } else {
       return (
-        <div>
-          <h2> {this.state.vehicle.name} </h2>
-          <ul>
-            <li> Model : {this.state.vehicle.model} </li>
-          </ul>
-        </div>
-      )
+        <Grid>
+          <Row className='vehicle-grid'>
+            <Col xs={12} className='text-center'>
+              <h2> {this.state.vehicle.name} </h2>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <h3> Construct </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Class : {this.state.vehicle.vehicle_class} </p>
+              <p> Model : {this.state.vehicle.model} </p>
+              <p> Manufacturer : {this.state.vehicle.manufacturer} </p>
+              <p> Cost : {this.state.vehicle.cost_in_credits} credits</p>
+            </Col>
+
+            <Col xs={12} className='text-justify'>
+              <h3> Specification </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Length : {this.state.vehicle.length} m</p>
+              <p> Crew : {this.state.vehicle.crew} </p>
+              <p> Passengers : {this.state.vehicle.passengers} </p>
+              <p> Capacity : {this.state.vehicle.cargo_capacity} kg</p>
+            </Col>
+
+            <Col xs={12} className='text-justify'>
+              <h3> Speed </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Speed in the atmosphere : {this.state.vehicle.max_atmosphering_speed} miles/hour</p>
+            </Col>
+          </Row>
+        </Grid>)
     }
   }
 }

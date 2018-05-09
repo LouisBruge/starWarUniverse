dac6d47133b474357195fa687ae11ea5c654223f
Louis Bruge <louis.bruge@gmail.com>
2018-05-09 12:00:14 +0200

    Use bootstrap component in the starship render

--- src/Starship.js
+++ src/Starship.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Loading from './Loading'
+import { Grid, Col, Row } from 'react-bootstrap'
 const URL_STARSHIP = 'https://swapi.co/api/starships/'
 
 export default class Starship extends React.Component {
@@ -37,23 +38,41 @@ export default class Starship extends React.Component {
       )
     } else {
       return (
-        <div>
-          <h2> {this.state.starship.name} </h2>
-          <p> Class : {this.state.starship.starship_class} </p>
-          <p> Model : {this.state.starship.model} </p>
-          <p> Manufacturer : {this.state.starship.manufacturer} </p>
+        <Grid>
+          <Row className='starship-grid'>
+            <Col xs={12} className='text-center'>
+              <h2> {this.state.starship.name} </h2>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <h3> Construct </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Class : {this.state.starship.starship_class} </p>
+              <p> Model : {this.state.starship.model} </p>
+              <p> Manufacturer : {this.state.starship.manufacturer} </p>
+              <p> Cost : {this.state.starship.cost_in_credits} credits</p>
+            </Col>
 
-          <p> Cost : {this.state.starship.cost_in_credits} credits</p>
+            <Col xs={12} className='text-justify'>
+              <h3> Specification </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Length : {this.state.starship.length} m</p>
+              <p> Crew : {this.state.starship.crew} </p>
+              <p> Passengers : {this.state.starship.passengers} </p>
+              <p> Capacity : {this.state.starship.cargo_capacity} kg</p>
+            </Col>
 
-          <p> Length : {this.state.starship.length} m</p>
-          <p> Crew : {this.state.starship.crew} </p>
-          <p> Passengers : {this.state.starship.passengers} </p>
-          <p> Capacity : {this.state.starship.cargo_capacity} kg</p>
-
-          <p> Speed in the atmosphere : {this.state.starship.max_atmosphering_speed} miles/hour</p>
-          <p> Hyperdrive : {this.state.starship.hyperdrive_rating}</p>
-          <p> MGLT : {this.state.starship.MGLT} AU</p>
-        </div>)
+            <Col xs={12} className='text-justify'>
+              <h3> Speed </h3>
+            </Col>
+            <Col xs={12} className='text-justify'>
+              <p> Speed in the atmosphere : {this.state.starship.max_atmosphering_speed} miles/hour</p>
+              <p> Hyperdrive : {this.state.starship.hyperdrive_rating}</p>
+              <p> MGLT : {this.state.starship.MGLT} AU</p>
+            </Col>
+          </Row>
+        </Grid>)
     }
   }
 }

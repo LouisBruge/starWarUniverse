8896516bd7302e83cc0aa04141c80afb4392a782
Louis Bruge <louis.bruge@gmail.com>
2018-05-09 16:00:12 +0200

    Create a _fetchVehicle method to get vehicles used by the personnage

--- src/Personnage.js
+++ src/Personnage.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
-import { Grid, Row, Col } from 'react-bootstrap'
+import React, {
+  Component
+} from 'react'
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap'
 import _urlToId from './_urlToId'
 
 const URL_PEOPLE = 'https://swapi.co/api/people/'
 
 export default class Personnage extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
-    let idPerso = () => { return this.props.name !== undefined ? this.props.name : this.props.match.params.id }
+    let idPerso = () => {
+      return this.props.name !== undefined ? this.props.name : this.props.match.params.id
+    }
     console.log(idPerso)
 
     this.state = {
@@ -22,7 +30,7 @@ export default class Personnage extends Component {
     }
   }
 
-  _fetchPersonnage (id = '1') {
+  _fetchPersonnage(id = '1') {
     fetch(URL_PEOPLE + id)
       .then((response) => {
         return response.json()
@@ -35,7 +43,7 @@ export default class Personnage extends Component {
 
         let specie = `https://swapi.co/api/species/${_urlToId(myJson.species[0])}`
         fetch(specie)
-          .then(function (response) {
+          .then(function(response) {
             return response.json()
           })
           .then((myJson) => {
@@ -48,33 +56,78 @@ export default class Personnage extends Component {
         this.setState({
           personnage: myJson
         })
+
+        this._fetchVehicle(this.state.personnage.vehicles)
+        console.log(this.state.vehicles)
       })
   }
 
-  componentWillMount () { this._fetchPersonnage(this.state.id) }
-  render () {
+
+  _fetchVehicle(request) {
+    request.forEach(vehicle => {
+      fetch(vehicle)
+        .then(response => {
+          return response.json()
+        })
+        .then(myJson => {
+          let vehicles = this.state.vehicles
+          vehicles.push(myJson.name)
+          this.setState({
+            vehicles: vehicles
+          })
+        })
+    })
+  }
+
+  componentWillMount() {
+    this._fetchPersonnage(this.state.id)
+  }
+  render() {
     if (this.state.isLoading) {
-      return (
-        <div>
-          <p> Loading...</p>
-        </div>
+      return ( <
+        div >
+        <
+        p > Loading... < /p> <
+        /div>
       )
     } else {
-      return (
-        <Grid fluid>
-          <Row className='Personnage'>
-            <Col xs={12}>
-              <h2 className='text-center'> {this.state.personnage.name} </h2>
-            </Col>
-            <Col xs={12}>
-              <p> born in : {this.state.personnage.birth_year} </p>
-              <p> Sex : {this.state.personnage.gender} </p>
-              <p> Specie : {this.state.specie} </p>
-              <p> Height : {this.state.personnage.height} cm </p>
-              <p> Weight : {this.state.personnage.mass} kg </p>
-            </Col>
-          </Row>
-        </Grid>
+      return ( <
+        Grid fluid >
+        <
+        Row className = 'Personnage' >
+        <
+        Col xs = {
+          12
+        } >
+        <
+        h2 className = 'text-center' > {
+          this.state.personnage.name
+        } < /h2> <
+        /Col> <
+        Col xs = {
+          12
+        } >
+        <
+        p > born in: {
+          this.state.personnage.birth_year
+        } < /p> <
+        p > Sex: {
+          this.state.personnage.gender
+        } < /p> <
+        p > Specie: {
+          this.state.specie
+        } < /p> <
+        p > Height: {
+          this.state.personnage.height
+        }
+        cm < /p> <
+        p > Weight: {
+          this.state.personnage.mass
+        }
+        kg < /p> <
+        /Col> <
+        /Row> <
+        /Grid>
       )
     }
   }

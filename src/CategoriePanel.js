25bcd0c6b34e85989473eb2bc3f3445fcd7b3b7f
Louis Bruge <louis.bruge@gmail.com>
2018-05-12 22:56:58 +0200

    Rewrite CategoriePanel component to use Bootstrap's component Media

--- src/CategoriePanel.js
+++ src/CategoriePanel.js
@@ -1,30 +1,36 @@
 import React from 'react'
-import { Grid, Row, Col, Button, Image } from 'react-bootstrap'
+import { Grid, Row, Media, Button, Image } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 export default class CategoriePanel extends React.Component {
   convertUrl () {
     return `/${this.props.title.toLowerCase()}`
   }
+
+  imagePath () {
+    return `/Images/${this.props.imageSrc}`
+  }
+
   render () {
+    let image = <img src={this.imagePath()} />
+    let imageMedia = this.props.left === true ? <Media.Left> {image} </Media.Left> : <Media.Right> {image} </Media.Right>
+
     return (
-      <Grid>
-        <Row>
-          <Col xs={4}>
-            <Image src={this.props.imageSrc} responsive className='center-block' />
-          </Col>
-          <Col xs={8}>
-            <h1 className='text-center'>
+      <Grid fluid>
+        <Media>
+          {imageMedia}
+          <Media.Body >
+            <Media.Heading className='text-center'>
               {this.props.title}
-            </h1>
+            </Media.Heading>
             <p className='text-justify'>
               {this.props.descriptif}
             </p>
             <LinkContainer to={this.convertUrl()} >
-              <Button className='center-block'>People</Button>
+              <Button className='center-block'>Explore {this.props.title} base</Button>
             </LinkContainer>
-          </Col>
-        </Row>
+          </Media.Body>
+        </Media>
       </Grid>
     )
   }
